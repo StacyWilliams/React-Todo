90377168f820c92b502f2c0869881bb232d22b09
StacyWilliams <stacymsiverd@gmail.com>
2019-10-19 04:35:07 -0500

    cleanup

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,20 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from "react";
+import TodoItem from "./Todo";
+
+
+const TodoList = props => {
+        const sortedList = props.todosArray.sort((a, b) => a.completed - b.completed);
+     return (
+        <div className="todoList">
+            {sortedList.map(item => (
+            <TodoItem key={item.id} item={item} toggleItem={props.toggleItem} />
+         ))}
+       </div>
+        );
+    };
+
+    
+export default TodoList;
